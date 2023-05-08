20b047e50bf34a568928ece8cde0561eff623f9a
Justin Su <injustsu@gmail.com>
2023-05-08 03:57:16 -0400

    Git: Configure
    Inspired by https://www.alchemists.io/articles/git_configuration

---
+++ fish/conf.d/abbr-git.fish
@@ -0,0 +1,84 @@
+status is-interactive; or exit
+
+abbr gad -- git add
+
+abbr gam -- git commit --amend --no-edit
+abbr --command=git am -- commit --amend --no-edit
+
+abbr game -- git commit --amend
+abbr --command=git ame -- commit --amend
+
+abbr gbr -- git branch
+abbr --command=git br -- branch
+
+abbr gcl -- git clone
+abbr --command=git cl -- clone
+
+abbr gclb -- git clone --filter=blob:none
+abbr --command=git clb -- clone --filter=blob:none
+abbr --command=git cloneb -- clone --filter=blob:none
+
+abbr gcm --set-cursor -- git commit -m \"%\"
+abbr --command=git cm --set-cursor -- commit -m \"%\"
+
+abbr gco -- git checkout
+abbr --command=git co -- checkout
+
+abbr gcp -- git cherry-pick
+abbr --command=git cp -- cherry-pick
+
+abbr gdf -- git diff -p --stat
+abbr --command=git df -- diff -p --stat
+
+abbr gdfc -- git diff -p --stat --cached
+abbr --command=git dfc -- diff -p --stat --cached
+abbr --command=git diffc -- diff -p --stat --cached
+
+abbr gdfu -- git diff -p --stat @{u}
+abbr --command=git dfu -- diff -p --stat @{u}
+abbr --command=git diffu -- diff -p --stat @{u}
+
+abbr gfe -- git fetch
+abbr --command=git fe -- fetch
+
+abbr ggr -- git grep --break --heading
+abbr --command=git gr -- grep --break --heading
+
+abbr glg -- git log
+
+abbr glgu -- git log @{u}
+abbr --command=git lgu -- log @{u}
+
+abbr glol -- git lol
+
+abbr glola -- git lol --all
+abbr --command=git lola -- lol --all
+
+abbr gpull -- git pull
+
+abbr gpush -- git push
+abbr gpushf -- git push --force-with-lease
+
+abbr grb -- git rebase
+abbr --command=git rb -- rebase
+
+abbr grba -- git rebase --abort
+abbr --command=git rba -- rebase --abort
+
+abbr grbc -- git rebase --continue
+abbr --command=git rbc -- rebase --continue
+
+abbr grbi -- git rebase -i
+abbr --command=git rbi -- rebase -i
+
+abbr gsh -- git show -p --stat
+abbr --command=git sh -- show -p --stat
+
+abbr gst -- git status
+abbr --command=git st -- status
+
+abbr gsw -- git switch
+abbr --command=git sw -- switch
+
+abbr gswc -- git switch -c
+abbr --command=git swc -- switch -c
