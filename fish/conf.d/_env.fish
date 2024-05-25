c6e1a57de5d559a89b23bf08affb34278d409520
Justin Su <injustsu@gmail.com>
2024-05-25 02:35:18 -0400

    fish: Install and add basic config

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -1,4 +1,12 @@
+set -x XDG_CONFIG_HOME ~/code/dotfiles
+
+fish_add_path -g \
+    ~/.local/bin \
+    ~/code/scripts
+
 set -x HOMEBREW_BUNDLE_NO_DESCRIBE 1
 set -x HOMEBREW_NO_ANALYTICS 1
 set -x HOMEBREW_NO_ENV_HINTS 1
 set -x HOMEBREW_NO_UPGRADE_QUIT_CASKS 1
+
+set -x VISUAL nano
