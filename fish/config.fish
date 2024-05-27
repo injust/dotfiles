81a9af0e0463ed64abd834fb4924605c39ddef2a
Justin Su <injustsu@gmail.com>
2024-05-27 04:50:09 -0400

    fish: Add `dotdot` abbreviation

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    abbr -a dotdot -r '^\.{2,}$' -f __multicd
     abbr -a ll -- ls -AFhl
     abbr -a wget wget2
 
---
+++ fish/functions/__multicd.fish
@@ -0,0 +1,3 @@
+function __multicd
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
