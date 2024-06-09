abe568cfd0fc38f9d56f016a584a637af95120da
Justin Su <injustsu@gmail.com>
2024-06-08 20:01:23 -0400

    Install and configure fonts

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -9,4 +9,7 @@ set -x HOMEBREW_NO_ANALYTICS 1
 set -x HOMEBREW_NO_ENV_HINTS 1
 set -x HOMEBREW_NO_UPGRADE_QUIT_CASKS 1
 
+# https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings
+set -x LESSUTFCHARDEF e000-e09f:w,e0a0-e0bf:p,e0c0-f8ff:w,f0001-fffff:w
+
 set -x VISUAL nano
