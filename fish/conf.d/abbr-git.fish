04ea56ee0c4ce651617508b5bb004c8d82cf5a69
Justin Su <injustsu@gmail.com>
2024-06-10 18:27:03 -0400

    Overhaul fish and Git aliases and add abbreviations

---
+++ fish/conf.d/abbr-git.fish
@@ -0,0 +1,49 @@
+if not status is-interactive
+    exit
+end
+
+abbr gad -- git add
+
+abbr gbr -- git branch
+__abbr_git br -- branch
+
+abbr gco -- git checkout
+__abbr_git co -- checkout
+
+abbr gcp -- git cherry-pick
+__abbr_git cp -- cherry-pick
+
+abbr gdf -- git diff
+__abbr_git df -- diff
+
+abbr gdfc -- git diff --cached
+__abbr_git dfc -- diff --cached
+__abbr_git diffc -- diff --cached
+
+abbr glg -- git log
+
+abbr glol -- git lol
+
+abbr glola -- git lol --all
+__abbr_git lola -- lol --all
+
+abbr grb -- git rebase
+__abbr_git rb -- rebase
+
+abbr grba -- git rebase --abort
+__abbr_git rba -- rebase --abort
+
+abbr grbc -- git rebase --continue
+__abbr_git rbc -- rebase --continue
+
+abbr grbi -- git rebase --interactive
+__abbr_git rbi -- rebase --interactive
+
+abbr gsh -- git show
+__abbr_git sh -- show
+
+abbr gst -- git status
+__abbr_git st -- status
+
+abbr gsw -- git switch
+__abbr_git sw -- switch
