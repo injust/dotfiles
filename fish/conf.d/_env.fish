21f8180e0e4ce5f814cdd7120d26da1e3769f9c3
Justin Su <injustsu@gmail.com>
2024-06-26 01:53:41 -0400

    Configure fzf and fzf.fish

---
+++ fish/conf.d/_env.fish
@@ -0,0 +1,6 @@
+set -x FZF_DEFAULT_COMMAND fd --hidden --no-require-git
+set -x FZF_DEFAULT_OPTS --layout=reverse --multi --highlight-line --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --scrollbar=▐ --prompt=❯
+
+set -x SSH_SK_PROVIDER /usr/lib/ssh-keychain.dylib
+
+set -x VISUAL nano
--- fish/conf.d/env.fish
+++
@@ -1,3 +0,0 @@
-set -x SSH_SK_PROVIDER /usr/lib/ssh-keychain.dylib
-
-set -x VISUAL nano
