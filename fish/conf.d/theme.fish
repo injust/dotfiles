96886297693d22d8bb2908aa7910d4c3337ccb43
Justin Su <injustsu@gmail.com>
2024-06-26 10:33:11 -0400

    fzf: Install Catppuccin theme

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -1,3 +1,6 @@
 status is-interactive; or exit
 
 fish_config theme choose catppuccin-mocha
+
+# Remove `bg` color to support transparent terminal background
+set -a FZF_DEFAULT_OPTS (string replace --regex 'bg:#[[:xdigit:]]{6},' '' <(status dirname)/../../catppuccin/fzf/themes/catppuccin-fzf-mocha.rc)
