c536240ce8eea97593c724ac7257ade2b61b8b0c
Justin Su <injustsu@gmail.com>
2024-06-26 10:33:11 -0400

    fzf: Install Catppuccin theme

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,4 +1,4 @@
 set -gx FZF_DEFAULT_COMMAND fd --hidden
-set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --highlight-line --preview-window=wrap
+set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --highlight-line --preview-window=wrap
 
 set -gx VISUAL nano
