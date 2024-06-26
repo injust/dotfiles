646c872c5b1f199501613b01a620b30f47466bde
Justin Su <injustsu@gmail.com>
2024-06-26 01:53:41 -0400

    Configure fzf and fzf.fish

---
+++ fish/conf.d/fzf.fish
@@ -0,0 +1,16 @@
+status is-interactive || exit
+
+fzf_configure_bindings --directory=\cf --git_log=\cg --git_status= --processes=
+
+set fzf_fd_opts --hidden
+
+set fzf_git_log_format '%C(bold blue)%h%C(reset) %C(cyan)%<(16)%ah%C(reset) %C(yellow)%d%C(reset) %C(normal)%s%C(reset)  %C(dim normal)[%an]%C(reset)'
+
+set fzf_history_time_format '%a %b %e %H:%M'
+
+set fzf_directory_opts --height=90% --border
+set fzf_git_log_opts --height=90% --border --preview-window=default
+set fzf_git_status_opts --height=90% --border
+set fzf_history_opts --height=50% --min-height=16 --border
+set fzf_processes_opts --height=90% --border
+set fzf_variables_opts --height=90% --border
