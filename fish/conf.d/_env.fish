e4dee44917592420cef7f5bd5d86505dfc759c9f
Justin Su <injustsu@gmail.com>
2024-06-26 01:53:41 -0400

    Configure fzf and fzf.fish

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -6,6 +6,9 @@ fish_add_path -g \
 
 set -x COLORTERM truecolor
 
+set -x FZF_DEFAULT_COMMAND fd --hidden --no-require-git
+set -x FZF_DEFAULT_OPTS_FILE $XDG_CONFIG_HOME/.fzfrc
+
 # https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings
 set -x LESSUTFCHARDEF e000-e09f:w,e0a0-e0bf:p,e0c0-f8ff:w,f0001-fffff:w
 
