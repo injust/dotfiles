9e2155570225cab6a16371e8103beb63dddfa95b
Justin Su <injustsu@gmail.com>
2024-06-26 01:53:41 -0400

    Configure fzf and fzf.fish

---
+++ fish/conf.d/_env.fish
@@ -0,0 +1,4 @@
+set -x FZF_DEFAULT_COMMAND fd --hidden --no-require-git
+set -x FZF_DEFAULT_OPTS --layout=reverse --multi --highlight-line --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --scrollbar=▐ --prompt=❯
+
+set -x VISUAL nano
