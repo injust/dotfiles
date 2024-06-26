fc207f4d561415e3e4471a6ea2fd13fa8378ace5
Justin Su <injustsu@gmail.com>
2024-06-26 10:33:11 -0400

    fzf: Install Catppuccin theme

---
+++ fish/conf.d/theme.fish
@@ -0,0 +1,4 @@
+status is-interactive || exit
+
+# https://github.com/catppuccin/fzf/blob/6c1280db15a247e28bd99c1a9cc9f6719e5124f7/themes/catppuccin-fzf-mocha.fish with `bg` option removed (to support transparent terminal background)
+set -a FZF_DEFAULT_OPTS --color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --color=border:#6C7086,label:#cdd6f4
