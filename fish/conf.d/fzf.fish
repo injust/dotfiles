74f3946da1bfbc4a9c69df1e346a7de335aa3863
Justin Su <injustsu@gmail.com>
2024-06-26 01:53:41 -0400

    Configure fzf and fzf.fish

---
+++ fish/conf.d/fzf.fish
@@ -0,0 +1,10 @@
+status is-interactive || exit
+
+set fzf_fd_opts --hidden
+
+set fzf_directory_opts --height=90% --border
+set fzf_git_log_opts --height=90% --border --preview-window=default
+set fzf_git_status_opts --height=90% --border
+set fzf_history_opts --height=50% --min-height=16 --border
+set fzf_processes_opts --height=90% --border
+set fzf_variables_opts --height=90% --border
