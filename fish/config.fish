1b916dcc4ac0cffb941e50bd9c4131bb73db5a1c
Justin Su <injustsu@gmail.com>
2024-06-27 02:52:34 -0400

    Use Catppuccin theme for `LS_COLORS`

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     source ~/.config/fish/abbr.fish
     source ~/.config/fish/abbr-git.fish
 
+    set --global --export LS_COLORS (vivid generate catppuccin-mocha)
+
     starship init fish | source
     zoxide init fish | source
 end
