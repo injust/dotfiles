27dc886406b2fcfb38988656b1a09ada253a80aa
Justin Su <injustsu@gmail.com>
2024-06-27 02:52:34 -0400

    vivid: Install and use Catppuccin theme for $LS_COLORS

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -4,3 +4,5 @@ fish_config theme choose catppuccin-mocha
 
 # Remove `bg` color to support transparent terminal background
 set -gx FZF_DEFAULT_OPTS (string replace --regex 'bg:#[[:xdigit:]]{6},' '' <(status dirname)/../../catppuccin/fzf/themes/catppuccin-fzf-mocha.rc)
+
+set -x LS_COLORS (vivid generate catppuccin-mocha)
