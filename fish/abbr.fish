08ab7ef56a2b630a62c738f0973e4a4e6789e306
Justin Su <injustsu@gmail.com>
2024-06-28 08:19:45 -0400

    fish: Add `where` and `which` abbreviations

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -1,5 +1,7 @@
 abbr cat -- bat
 abbr wget -- wget2
+abbr where -- type --all
+abbr which -- type
 
 # Inspired by https://fishshell.com/docs/current/interactive.html#abbreviations
 abbr dotdot --regex='^\.{2,}$' --function=__multicd
