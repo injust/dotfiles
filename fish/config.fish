e196b5b8b2e05f954ff3ee5914e0b7622b663112
Justin Su <injustsu@gmail.com>
2024-06-28 04:10:22 -0400

    Add GNU findutils, GNU sed, and uutils-coreutils to PATH and MANPATH

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,8 @@ if status is-interactive
     starship init fish | source
     zoxide init fish | source
 end
+
+# Prepend manual paths corresponding to `fish_user_paths`
+# Without this, `man` prioritizes the default manual path over Homebrew-installed commands, e.g. coreutils
+# Trailing colon prepends this to the default list
+set --global --export MANPATH /usr/local/opt/findutils/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:/usr/local/opt/uutils-coreutils/libexec/uuman:
