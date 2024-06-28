6e4bee7416031c39aab8b990e981721593d1aa14
Justin Su <injustsu@gmail.com>
2024-06-28 08:47:05 -0400

    eza: Install and use in aliases

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --description='alias ll=ls -AFhl' --wraps=ls
-    ls -AFhl $argv
+function ll --description='alias ll=la -l --smart-group --git-repos' --wraps=eza
+    la --long --smart-group --git-repos $argv
 end
