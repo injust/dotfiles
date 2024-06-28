0a4c1ee4c3f67a83d68300cc627906d78ce7a875
Justin Su <injustsu@gmail.com>
2024-06-28 04:10:22 -0400

    Add GNU findutils, GNU sed, and uutils-coreutils to PATH

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,1 +1,4 @@
 fish_add_path -ag /usr/local/opt/curl/bin
+fish_add_path -ag /usr/local/opt/findutils/libexec/gnubin
+fish_add_path -ag /usr/local/opt/gnu-sed/libexec/gnubin
+fish_add_path -ag /usr/local/opt/uutils-coreutils/libexec/uubin
