894c4e8f59781164aba71ef070d0a493c32418b8
Justin Su <injustsu@gmail.com>
2024-06-29 01:29:00 -0400

    bat: Use as a colorizing pager for man
    Also removes backspace characters when `stdout` is not a terminal

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -3,4 +3,6 @@ set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ -
 # https://github.com/catppuccin/fzf/blob/fd3ab96c134abeca7721f16ad251bc2e9defa493/themes/catppuccin-fzf-mocha.fish with `bg` option removed (to support transparent terminal background)
 --color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --color=border:#313244,label:#cdd6f4
 
+set -gx MANPAGER sh -c "'col -bx | bat --language=man --style=plain'"
+
 set -gx VISUAL nano
