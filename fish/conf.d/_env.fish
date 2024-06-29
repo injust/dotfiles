35f13bb677f6e0188b578185b7bc4b9553db887f
Justin Su <injustsu@gmail.com>
2024-06-29 01:29:00 -0400

    bat: Use as a colorizing man pager
    https://github.com/sharkdp/bat/blob/master/README.md#man

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -1,6 +1,9 @@
 set -x FZF_DEFAULT_COMMAND fd --hidden --no-require-git
 set -x FZF_DEFAULT_OPTS --layout=reverse --multi --highlight-line --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --scrollbar=▐ --prompt=❯
 
+# https://github.com/sharkdp/bat/blob/master/README.md#man
+set -x MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
+
 set -x SSH_SK_PROVIDER /usr/lib/ssh-keychain.dylib
 
 set -x VISUAL nano
