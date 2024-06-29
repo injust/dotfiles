18bf8e2833496b5b87fcf542b819a682e4859cb0
Justin Su <injustsu@gmail.com>
2024-06-28 20:12:18 -0400

    Install GNU awk, grep, make, tar, units, and which, and modern versions of file, gzip, less, and openssh

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,4 +1,11 @@
 fish_add_path -ag /usr/local/opt/curl/bin
+fish_add_path -ag /usr/local/opt/file-formula/bin
 fish_add_path -ag /usr/local/opt/findutils/libexec/gnubin
+fish_add_path -ag /usr/local/opt/gawk/libexec/gnubin
 fish_add_path -ag /usr/local/opt/gnu-sed/libexec/gnubin
+fish_add_path -ag /usr/local/opt/gnu-tar/libexec/gnubin
+fish_add_path -ag /usr/local/opt/gnu-units/libexec/gnubin
+fish_add_path -ag /usr/local/opt/gnu-which/libexec/gnubin
+fish_add_path -ag /usr/local/opt/grep/libexec/gnubin
+fish_add_path -ag /usr/local/opt/make/libexec/gnubin
 fish_add_path -ag /usr/local/opt/uutils-coreutils/libexec/uubin
