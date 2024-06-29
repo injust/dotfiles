bf667c23318a6bcc2e11c21d15b697133ecf961b
Justin Su <injustsu@gmail.com>
2024-06-28 20:12:18 -0400

    Install GNU awk, coreutils, findutils, grep, sed, tar, units, watch, and which

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,1 +1,4 @@
+# Homebrew
+fish_add_path -g --path /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/findutils/libexec/gnubin /usr/local/opt/gawk/libexec/gnubin /usr/local/opt/gnu-sed/libexec/gnubin /usr/local/opt/gnu-tar/libexec/gnubin /usr/local/opt/gnu-units/libexec/gnubin /usr/local/opt/gnu-which/libexec/gnubin /usr/local/opt/grep/libexec/gnubin
+
 fish_add_path -g ~/.local/bin ~/code/scripts
