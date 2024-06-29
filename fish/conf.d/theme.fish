aea795c18f8c4c547480963815fcaee1ab79a5b4
Justin Su <injustsu@gmail.com>
2024-06-29 01:18:53 -0400

    bat: Install Catppuccin theme

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -8,4 +8,11 @@ fish_config theme choose 'Catppuccin Mocha'
 # - Remove `bg` color to support transparent terminal background
 set -a FZF_DEFAULT_OPTS --color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8,selected-bg:#45475a,border:#6c7086,label:#cdd6f4
 
+# https://github.com/junegunn/fzf/issues/4549
+if command -q /usr/bin/defaults
+    set -a fzf_preview_file_cmd --theme=auto:system
+else
+    set -a fzf_preview_file_cmd --theme=dark
+end
+
 set -gx LS_COLORS (vivid generate catppuccin-mocha)
