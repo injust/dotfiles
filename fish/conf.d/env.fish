c5c6e143a346909123cc4d1fb45852526009ec4b
Justin Su <injustsu@gmail.com>
2024-06-29 01:29:00 -0400

    bat: Use as a colorizing pager for man
    https://github.com/sharkdp/bat/blob/master/README.md#man

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,4 +1,8 @@
 set -gx FZF_DEFAULT_COMMAND fd --hidden
 set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --highlight-line --preview-window=wrap
 
+# https://github.com/sharkdp/bat/blob/f754f43e84a275d7a9a0ee3544c485eed747b311/README.md#man
+# TODO(bat v0.26.2): Simplify to `set -gx MANPAGER bat --plain --language=man` (https://github.com/sharkdp/bat/blob/59c58969024d48c0575c42250c7dd87a70106fbd/README.md#man)
+set -gx MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
+
 set -gx VISUAL nano
