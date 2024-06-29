9c128e845d7c0db4123ba6cbc2e097859929c8de
Justin Su <injustsu@gmail.com>
2024-06-28 20:42:21 -0400

    fish: Add grep alias for default options
    Unlike ls, grep `--color` defaults to `--color=auto`.

---
+++ fish/functions/grep.fish
@@ -0,0 +1,3 @@
+function grep --description='alias grep=grep --color --line-number --binary-files=without-match --devices=skip'
+    command grep --color --line-number --binary-files=without-match --devices=skip $argv
+end
