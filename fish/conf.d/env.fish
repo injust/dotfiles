637e33ca5d452c27856d3914fc9aa1de41a5333d
Justin Su <injustsu@gmail.com>
2024-06-29 01:29:00 -0400

    bat: Use as a colorizing pager for man
    Also removes backspace characters when `stdout` is not a terminal

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,4 +1,6 @@
 set -gx FZF_DEFAULT_COMMAND fd --hidden
 set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --highlight-line --preview-window=wrap
 
+set -gx MANPAGER sh -c "'col -bx | bat --language=man --style=plain'"
+
 set -gx VISUAL nano
