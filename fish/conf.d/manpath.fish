9a970ea75c8526b72ef3bb48abbe7d3e1367fe9f
Justin Su <injustsu@gmail.com>
2024-06-28 20:12:18 -0400

    Install GNU grep and add to PATH and MANPATH

--- fish/conf.d/manpath.fish
+++ fish/conf.d/manpath.fish
@@ -1,4 +1,4 @@
 # Prepend manual paths corresponding to `fish_user_paths`
 # Without this, `man` prioritizes the default manual path over Homebrew-installed commands, e.g. coreutils
 # Trailing colon prepends this to the default list
-set --global --export MANPATH /usr/local/opt/findutils/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:/usr/local/opt/uutils-coreutils/libexec/uuman:
+set --global --export MANPATH /usr/local/opt/findutils/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:/usr/local/opt/grep/libexec/gnuman:/usr/local/opt/uutils-coreutils/libexec/uuman:
