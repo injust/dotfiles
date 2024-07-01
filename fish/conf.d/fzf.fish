6b4e613b5e259d92ecbf2b3a8e391c9794d61591
Justin Su <injustsu@gmail.com>
2024-07-01 10:00:49 -0400

    delta: Install

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -2,6 +2,8 @@ status is-interactive; or exit
 
 fzf_configure_bindings --directory=ctrl-f --git-log=ctrl-g --git-status= --history=ctrl-r --processes=ctrl-p --variables=ctrl-v
 
+set fzf_diff_highlighter delta --width=\$FZF_PREVIEW_COLUMNS
+
 set fzf_fd_opts --hidden --no-require-git
 
 set fzf_git_log_format '%C(bold blue)%h%C(reset) %C(cyan)%<(16)%ah%C(reset) %C(yellow)%d%C(reset) %C(normal)%s%C(reset)  %C(dim normal)[%an]%C(reset)'
