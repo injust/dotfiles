9f08f8197743047f13a32522c2fc5f56adc4e04a
Justin Su <injustsu@gmail.com>
2024-07-01 06:29:46 -0400

    Install GNU which and add to PATH and MANPATH

--- fish/conf.d/manpath.fish
+++ fish/conf.d/manpath.fish
@@ -1,4 +1,4 @@
 # Prepend manual paths corresponding to `fish_user_paths`
 # Without this, `man` prioritizes the default manual path over Homebrew-installed commands, e.g. coreutils
 # Trailing colon prepends this to the default list
-set -gx MANPATH /usr/local/opt/findutils/libexec/gnuman:/usr/local/opt/gawk/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:/usr/local/opt/gnu-tar/libexec/gnuman:/usr/local/opt/grep/libexec/gnuman:/usr/local/opt/uutils-coreutils/libexec/uuman:
+set -gx MANPATH /usr/local/opt/findutils/libexec/gnuman:/usr/local/opt/gawk/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:/usr/local/opt/gnu-tar/libexec/gnuman:/usr/local/opt/gnu-which/libexec/gnuman:/usr/local/opt/grep/libexec/gnuman:/usr/local/opt/uutils-coreutils/libexec/uuman:
