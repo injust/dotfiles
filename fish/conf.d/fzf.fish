2879cd0abd5a249fdff62bae97bab90a2e663b0a
Justin Su <injustsu@gmail.com>
2024-07-01 09:45:55 -0400

    fzf.fish: Configure keybinds and git log format

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -2,10 +2,13 @@ if not status is-interactive
     exit
 end
 
+fzf_configure_bindings --directory=\cf --git_log=\cg --git_status= --processes=
+
 set fzf_directory_opts --height=90% --border
 
 set fzf_fd_opts --hidden
 
+set fzf_git_log_format '%C(bold blue)%h%C(reset) %C(cyan)%<(16)%ah%C(reset) %C(yellow)%d%C(reset) %C(normal)%s%C(reset)  %C(dim normal)[%an]%C(reset)'
 set fzf_git_log_opts --height=90% --border --preview-window=default
 
 set fzf_git_status_opts --height=90% --border
