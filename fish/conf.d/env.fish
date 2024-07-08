6dd40f9c2376e29b9e8c868a24938780359bed12
Justin Su <injustsu@gmail.com>
2024-07-08 06:06:29 -0400

    Install GNU units and add to PATH and MANPATH

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -5,6 +5,6 @@ set -gx MANPAGER sh -c "'col -bx | bat --language=man --style=plain'"
 # Prepend manual paths corresponding to `fish_user_paths`
 # Without this, `man` prioritizes the default manual path over Homebrew-installed commands, e.g. coreutils
 # Trailing colon prepends this to the default list
-set -gx MANPATH /usr/local/opt/findutils/libexec/gnuman:/usr/local/opt/gawk/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:/usr/local/opt/gnu-tar/libexec/gnuman:/usr/local/opt/gnu-which/libexec/gnuman:/usr/local/opt/grep/libexec/gnuman:/usr/local/opt/uutils-coreutils/libexec/uuman:
+set -gx MANPATH /usr/local/opt/findutils/libexec/gnuman:/usr/local/opt/gawk/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:/usr/local/opt/gnu-tar/libexec/gnuman:/usr/local/opt/gnu-units/libexec/gnuman:/usr/local/opt/gnu-which/libexec/gnuman:/usr/local/opt/grep/libexec/gnuman:/usr/local/opt/uutils-coreutils/libexec/uuman:
 
 set -gx VISUAL nano
