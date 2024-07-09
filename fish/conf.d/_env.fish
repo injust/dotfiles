abdce4bef94050f378fbfa02cbea22faadc2d034
Justin Su <injustsu@gmail.com>
2024-07-09 08:20:05 -0400

    less: Override Git's `LESS=FRX` default to unbreak mouse scrolling
    https://redirect.github.com/gwsw/less/issues/445

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -1,6 +1,8 @@
 set -x FZF_DEFAULT_COMMAND fd --hidden --no-require-git
 set -x FZF_DEFAULT_OPTS --layout=reverse --multi --highlight-line --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --scrollbar=▐ --prompt=❯
 
+set -x LESS --quit-if-one-screen --RAW-CONTROL-CHARS
+
 # https://github.com/sharkdp/bat/blob/master/README.md#man
 set -x MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
 
