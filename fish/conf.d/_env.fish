aa83819f17e5d110d9656d6106936af418e60fb7
Justin Su <injustsu@gmail.com>
2024-07-09 08:20:05 -0400

    less: Override Git's `LESS=FRX` default to unbreak mouse scrolling
    https://redirect.github.com/gwsw/less/issues/445

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -16,6 +16,7 @@ set -x COLORTERM truecolor
 set -x FZF_DEFAULT_COMMAND fd --hidden --no-require-git
 set -x FZF_DEFAULT_OPTS_FILE $XDG_CONFIG_HOME/.fzfrc
 
+set -x LESS --quit-if-one-screen --RAW-CONTROL-CHARS
 # https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings
 set -x LESSUTFCHARDEF e000-e09f:w,e0a0-e0bf:p,e0c0-f8ff:w,f0001-fffff:w
 
