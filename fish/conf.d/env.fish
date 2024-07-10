76fda92169ec181b2a0d3fed7db3bc9a7485f079
Justin Su <injustsu@gmail.com>
2024-07-10 13:28:32 -0400

    micro: Configure and use as default editor

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -6,4 +6,4 @@ set -x LESS --quit-if-one-screen --RAW-CONTROL-CHARS
 # https://github.com/sharkdp/bat/blob/master/README.md#man
 set -x MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
 
-set -x VISUAL nano
+set -x VISUAL micro
