e7b76184984aaebb162af5128888bed2ab5fc5af
Justin Su <injustsu@gmail.com>
2024-07-10 13:28:32 -0400

    micro: Configure and use as default editor

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -5,4 +5,4 @@ set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ -
 # TODO(bat v0.26.2): Simplify to `set -gx MANPAGER bat --plain --language=man` (https://github.com/sharkdp/bat/blob/59c58969024d48c0575c42250c7dd87a70106fbd/README.md#man)
 set -gx MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
 
-set -gx VISUAL nano
+set -gx VISUAL micro
