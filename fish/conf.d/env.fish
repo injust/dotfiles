04dbb673f9e4a45d6092f0465cb29f71a22021e4
Justin Su <injustsu@gmail.com>
2024-07-18 19:21:44 -0400

    direnv: Install and configure for uv

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,3 +1,5 @@
+set -gx DIRENV_LOG_FORMAT
+
 set -gx FZF_DEFAULT_COMMAND fd --hidden --follow --type=file
 set -gx FZF_DEFAULT_OPTS --layout=reverse --scrollbar=▐ --prompt="'❯ '" --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --color=fg:#cdd6f4,bg:#1e1e2e,hl:#f38ba8,fg+:#cdd6f4,bg+:#313244,hl+:#f38ba8,info:#cba6f7,prompt:#cba6f7,pointer:#f5e0dc,marker:#89b4fa,spinner:#f5e0dc,header:#f38ba8 --highlight-line --preview-window=wrap
 
