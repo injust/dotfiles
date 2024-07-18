df056641d51909f0cd82c40fa277f1f3b84643c6
Justin Su <injustsu@gmail.com>
2024-07-18 19:21:44 -0400

    direnv: Install, configure for uv, and disable automatic venv activation in IDE

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,3 +1,6 @@
+# TODO(https://github.com/direnv/direnv/issues/68, https://github.com/direnv/direnv/pull/1231): Use a less hacky solution to suppress logging so `log_status` still works
+set -gx DIRENV_LOG_FORMAT
+
 set -gx FZF_DEFAULT_COMMAND fd --hidden
 set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --highlight-line --preview-window=wrap
 
