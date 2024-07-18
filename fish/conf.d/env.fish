ac060c746adf494992246f53d06f1da909aa8cf0
Justin Su <injustsu@gmail.com>
2024-07-18 19:20:01 -0400

    Homebrew: Add a TODO

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -5,6 +5,7 @@ set -gx MANPAGER sh -c "'col -bx | bat --language=man --style=plain'"
 # Prepend manual paths corresponding to `fish_user_paths`
 # Without this, `man` prioritizes the default manual path over Homebrew-installed commands, e.g. coreutils
 # Trailing colon prepends this to the default list
+# TODO(https://github.com/Homebrew/homebrew-core/issues/176037, https://github.com/Homebrew/homebrew-core/pull/176589, https://github.com/Homebrew/homebrew-core/pull/176875): These should now be automatically prepended, unset MANPATH if it works
 set -gx MANPATH /usr/local/opt/findutils/libexec/gnuman:/usr/local/opt/gawk/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:/usr/local/opt/gnu-tar/libexec/gnuman:/usr/local/opt/gnu-units/libexec/gnuman:/usr/local/opt/gnu-which/libexec/gnuman:/usr/local/opt/grep/libexec/gnuman:/usr/local/opt/uutils-coreutils/libexec/uuman:
 
 set -gx MICRO_TRUECOLOR 1
