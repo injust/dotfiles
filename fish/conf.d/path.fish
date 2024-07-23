18e1a690a360a2afe1d86b04c944336875983eec
Justin Su <injustsu@gmail.com>
2024-07-23 01:12:00 -0400

    fish: Add user paths

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,1 +1,4 @@
+# Homebrew
 fish_add_path -g --path /usr/local/opt/curl/bin /usr/local/opt/file-formula/bin /usr/local/opt/findutils/libexec/gnubin /usr/local/opt/gawk/libexec/gnubin /usr/local/opt/gnu-sed/libexec/gnubin /usr/local/opt/gnu-tar/libexec/gnubin /usr/local/opt/gnu-units/libexec/gnubin /usr/local/opt/gnu-which/libexec/gnubin /usr/local/opt/grep/libexec/gnubin /usr/local/opt/make/libexec/gnubin /usr/local/opt/uutils-coreutils/libexec/uubin
+
+fish_add_path -g ~/code/scripts ~/.local/bin
