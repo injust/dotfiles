d77144df1d09253905351f5220df6a5854d15e5c
Justin Su <injustsu@gmail.com>
2024-08-04 04:15:25 -0400

    Git: Add commit message to `git cm` alias and `gcm` abbreviation

--- fish/conf.d/abbr-git.fish
+++ fish/conf.d/abbr-git.fish
@@ -2,25 +2,29 @@ status is-interactive || exit
 
 # Inspired by https://github.com/paulirish/dotfiles/blob/3412844c7a0a768864a0ac384731721de1dd9c2e/fish/aliases.fish#L44
 function abbr_git
-    argparse -- $argv; or return
+    argparse set-cursor -- $argv; or return
     set -l short $argv[1]
     set -l long $argv[2..]
+    set -l options (set -q _flag_set_cursor; and echo -- --set-cursor)
 
     if not string match --regex --quiet '^\w[\-\w]*$' -- "$short"
-        or not string match --regex --quiet '^[\-@{}\h\w]+$' -- "$long"
+        or not string match --regex --quiet '^["%\-=@{}\h\w]+$' -- "$long"
         return 1
     end
+
+    set -l escaped_short (string escape -- $short)
+    set -l escaped_long (string escape -- $long)
 
     eval "function __git_$short
         set -l tokens (commandline --tokenize)
         if test \$tokens[1] = git
-            echo -- $long
+            echo -- $escaped_long
         else
-            echo -- $short
+            echo -- $escaped_short
         end
     end"
 
-    abbr $short --position=anywhere --function=__git_$short
+    abbr $short --position=anywhere $options --function=__git_$short
 end
 
 abbr gad -- git add
@@ -38,8 +42,8 @@ abbr_git br -- branch
 abbr gcl -- git clone
 abbr_git cl -- clone
 
-abbr gcm -- git commit
-abbr_git cm -- commit
+abbr gcm --set-cursor -- git commit --message='"%"'
+abbr_git cm --set-cursor -- commit --message='"%"'
 
 abbr gco -- git checkout
 abbr_git co -- checkout
