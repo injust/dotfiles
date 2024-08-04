4185f16d83b31c0516ae276986dd3bde715f472b
Justin Su <injustsu@gmail.com>
2024-08-04 04:15:25 -0400

    Git: Add commit messages for `git cm` and `gcm` fish abbreviations

--- fish/functions/__abbr_git.fish
+++ fish/functions/__abbr_git.fish
@@ -1,22 +1,25 @@
 # Inspired by https://github.com/paulirish/dotfiles/blob/main/fish/aliases.fish
 function __abbr_git
-    argparse -- $argv; or return
+    argparse set-cursor -- $argv; or return
     set -l short $argv[1]
     set -l long $argv[2..-1]
 
     if not string match --regex --quiet '^\w[\-\w]*$' -- $short
-        or not string match --regex --quiet '^[\-@{}\h\w]+$' -- "$long"
+        or not string match --regex --quiet '^["%\-=@{}\h\w]+$' -- "$long"
         return 1
     end
+
+    set -l escaped_short (string escape -- $short)
+    set -l escaped_long (string escape -- $long)
 
     eval "function __git_$short
         set -l tokens (commandline --tokenize)
         if test \$tokens[1] = git
-            echo $long
+            echo $escaped_long
         else
-            echo $short
+            echo $escaped_short
         end
     end"
 
-    abbr $short --position=anywhere --function=__git_$short
+    abbr $short --position=anywhere (set -q _flag_set_cursor; and echo -- --set-cursor) --function=__git_$short
 end
