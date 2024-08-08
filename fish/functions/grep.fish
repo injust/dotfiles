874f0bdf4ce557177e2f34a290e11d1dd272e07b
Justin Su <injustsu@gmail.com>
2024-08-08 08:00:24 -0400

    fish: Use short options in alias descriptions

--- fish/functions/grep.fish
+++ fish/functions/grep.fish
@@ -1,3 +1,3 @@
-function grep --description='alias grep=grep --color --line-number --binary-files=without-match --devices=skip'
+function grep --description='alias grep=grep --color -n --binary-files=without-match -D skip'
     command grep --color --line-number --binary-files=without-match --devices=skip $argv
 end
