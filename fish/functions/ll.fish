e6c7155c61dba438365c1ac6335d76b38a6d555e
Justin Su <injustsu@gmail.com>
2024-08-08 07:02:44 -0400

    eza: Use `--git-repos` in `ll` alias

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --description='alias ll=ls --long --all' --wraps=eza
-    ls --long --all $argv
+function ll --description='alias ll=ls --long --all --git-repos' --wraps=eza
+    ls --long --all --git-repos $argv
 end
