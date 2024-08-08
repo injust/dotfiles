efb6885060dea338604933181d276f35ad5afba1
Justin Su <injustsu@gmail.com>
2024-08-08 08:00:24 -0400

    fish: Use short options in alias descriptions

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --description='alias ll=ls --long --all --smart-group --git-repos' --wraps=eza
+function ll --description='alias ll=ls -la --smart-group --git-repos' --wraps=eza
     ls --long --all --smart-group --git-repos $argv
 end
