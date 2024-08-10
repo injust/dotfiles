69602e90acf450f93b86cf8961ec4dcca35feabc
Justin Su <injustsu@gmail.com>
2024-08-10 06:29:17 -0400

    fish: Add `fd` alias for default options

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,4 +1,4 @@
-set -gx FZF_DEFAULT_COMMAND fd --hidden
+set -gx FZF_DEFAULT_COMMAND fd
 set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --highlight-line --preview-window=wrap
 
 set -gx MANPAGER sh -c "'col -bx | bat --language=man --style=plain'"
