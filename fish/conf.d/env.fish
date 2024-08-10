77f4c47139df3f4536d4e0e84f80b3094746bbd2
Justin Su <injustsu@gmail.com>
2024-08-10 05:41:44 -0400

    fzf: Tweak Catppuccin theme to work with translucent terminal background
    https://redirect.github.com/catppuccin/fzf/issues/9
    https://redirect.github.com/catppuccin/fzf/pull/13

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -4,7 +4,7 @@ set -gx DIRENV_LOG_FORMAT
 set -gx FZF_DEFAULT_COMMAND fd --hidden
 set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --highlight-line --preview-window=wrap \
 # https://github.com/catppuccin/fzf/blob/main/README.md
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --color=border:#313244,label:#cdd6f4
+--color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --color=border:#313244,label:#cdd6f4
 
 set -gx MANPAGER sh -c "'col -bx | bat --language=man --style=plain'"
 
