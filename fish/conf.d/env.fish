19dc6fbf4fa03e4caa5a2df4047ba0704eeb3daa
Justin Su <injustsu@gmail.com>
2024-08-10 06:29:17 -0400

    fish: Add `fd` alias for default options

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,4 +1,4 @@
-set -x FZF_DEFAULT_COMMAND fd --hidden --no-require-git
+set -x FZF_DEFAULT_COMMAND fd
 set -x FZF_DEFAULT_OPTS --layout=reverse --multi --highlight-line --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --scrollbar=▐ --prompt=❯
 
 set -x LESS --quit-if-one-screen --RAW-CONTROL-CHARS
