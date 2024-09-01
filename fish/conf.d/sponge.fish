31a8b78c3b212bd7241ba18c62e7eb019289276f
Justin Su <injustsu@gmail.com>
2024-09-01 00:59:38 -0400

    fish: Configure Sponge

---
+++ fish/conf.d/sponge.fish
@@ -0,0 +1,10 @@
+status is-interactive; or exit
+
+set sponge_purge_only_on_exit true
+set sponge_regex_patterns \
+    '\bgit .*\b[\da-f]{7,40}\b' \
+    '\bgit commit -m\b' \
+    ' https://instagram\.\w+-\d\.fna\.fbcdn\.net/' \
+    ' https://scontent-\w+-\d\.cdninstagram\.com/' \
+    '^man ' \
+    '^mv out\.\w+ '
