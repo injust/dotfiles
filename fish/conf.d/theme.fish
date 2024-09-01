146b019713d85e0a49f1b376c8e4a1f46f9a0e8b
Justin Su <injustsu@gmail.com>
2024-08-31 21:50:25 -0400

    delta: Work around ignored line number styles
    https://redirect.github.com/dandavison/delta/issues/1837

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -2,6 +2,9 @@ status is-interactive; or exit
 
 fish_config theme choose catppuccin-mocha
 
+# Work around ignored line number styles (https://github.com/dandavison/delta/issues/1837)
+set -gx DELTA_FEATURES +catppuccin-mocha
+
 # Remove `bg` color to support transparent terminal background
 set -gx FZF_DEFAULT_OPTS (string replace --regex 'bg:#[[:xdigit:]]{6},' '' <(status dirname)/../../catppuccin/fzf/themes/catppuccin-fzf-mocha.rc)
 
