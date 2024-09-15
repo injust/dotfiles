f3284df0e7c7815e8259f1d61feaa482e68f3a64
Justin Su <injustsu@gmail.com>
2024-09-15 14:46:18 -0400

    pre-commit: Silence missing config file error

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -7,4 +7,6 @@ set -gx LESS --quit-if-one-screen --RAW-CONTROL-CHARS
 # TODO(bat v0.26.2): Simplify to `bat --plain --language=man` (https://github.com/sharkdp/bat/blob/59c58969024d48c0575c42250c7dd87a70106fbd/README.md#man)
 set -gx MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
 
+set -gx PRE_COMMIT_ALLOW_NO_CONFIG 1
+
 set -gx VISUAL micro
