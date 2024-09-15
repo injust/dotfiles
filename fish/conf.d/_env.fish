4f08d796568bfc4f60f0bda9fc6c7f49ffe0b35d
Justin Su <injustsu@gmail.com>
2024-09-15 16:35:59 -0400

    ripgrep: Install and configure

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -6,6 +6,8 @@ set -x LESS --quit-if-one-screen --RAW-CONTROL-CHARS
 # https://github.com/sharkdp/bat/blob/master/README.md#man
 set -x MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
 
+set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/config
+
 set -x SSH_SK_PROVIDER /usr/lib/ssh-keychain.dylib
 
 set -x VISUAL micro
