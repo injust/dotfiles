cf5bf1ef604fbb90b5e777d33e6e0dcff7fc79ec
Justin Su <injustsu@gmail.com>
2024-09-17 20:09:39 -0400

    Amazon Q: Uninstall

--- fish/conf.d/00_fig_pre.fish
+++
@@ -1,3 +0,0 @@
-if status is-interactive; and test "$TERM_PROGRAM" = vscode
-    test -x ~/.local/bin/q; and eval (~/.local/bin/q init fish pre --rcfile 00_fig_pre | string split0)
-end
--- fish/conf.d/99_fig_post.fish
+++
@@ -1,3 +0,0 @@
-if status is-interactive; and test "$TERM_PROGRAM" = vscode
-    test -x ~/.local/bin/q; and eval (~/.local/bin/q init fish post --rcfile 99_fig_post | string split0)
-end
