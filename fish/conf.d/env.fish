27eba98d68bc6b85b64af1e7f86e833e9514dad1
Justin Su <injustsu@gmail.com>
2024-09-20 03:19:28 -0400

    Use smart-case search

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -2,7 +2,7 @@ set -gx FZF_DEFAULT_COMMAND fd
 set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --highlight-line --preview-window=wrap
 
 # Override Git's `LESS=FRX` default to unbreak mouse scrolling (https://github.com/gwsw/less/issues/445)
-set -gx LESS --quit-if-one-screen --RAW-CONTROL-CHARS
+set -gx LESS --quit-if-one-screen --ignore-case --RAW-CONTROL-CHARS
 
 # https://github.com/sharkdp/bat/blob/f754f43e84a275d7a9a0ee3544c485eed747b311/README.md#man
 set -gx MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat -p -lman'"
