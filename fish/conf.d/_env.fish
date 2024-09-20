3d3b3f5eec31576d3c27b93d34c6a2e827055516
Justin Su <injustsu@gmail.com>
2024-09-20 03:32:18 -0400

    less: Configure

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -1,7 +1,8 @@
 set -x FZF_DEFAULT_COMMAND fd
 set -x FZF_DEFAULT_OPTS --layout=reverse --multi --highlight-line --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --scrollbar=▐ --prompt=❯
 
-set -x LESS --quit-if-one-screen --ignore-case --RAW-CONTROL-CHARS
+set -x LESS --quit-if-one-screen --ignore-case --jump-target=.1 --LONG-PROMPT --quiet --RAW-CONTROL-CHARS --incsearch --no-vbell
+set -x LESSSECURE 1
 
 # https://github.com/sharkdp/bat/blob/master/README.md#man
 set -x MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
