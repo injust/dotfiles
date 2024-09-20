790b06507c13f3d526f886e598ea88c854db223b
Justin Su <injustsu@gmail.com>
2024-09-20 03:04:36 -0400

    less: Move default options from Git config to $LESS environment variable

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -6,6 +6,9 @@ set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ -
 # https://github.com/catppuccin/fzf/blob/fd3ab96c134abeca7721f16ad251bc2e9defa493/themes/catppuccin-fzf-mocha.fish with `bg` option removed (to support transparent terminal background)
 --color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --color=border:#313244,label:#cdd6f4
 
+# Override Git's `LESS=FRX` default to unbreak mouse scrolling (https://github.com/gwsw/less/issues/445)
+set -gx LESS --quit-if-one-screen --RAW-CONTROL-CHARS
+
 set -gx MANPAGER sh -c "'col -bx | bat --language=man --style=plain'"
 
 # TODO(micro 2.0.15): Remove this, micro automatically uses true color support now (https://github.com/zyedidia/micro/pull/2867)
