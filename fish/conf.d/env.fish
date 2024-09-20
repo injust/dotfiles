bd46cc5c14a2e7416820773de603a7a77f5b589b
Justin Su <injustsu@gmail.com>
2024-09-20 03:19:28 -0400

    Use smart-case search

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,7 +1,7 @@
 set -x FZF_DEFAULT_COMMAND fd
 set -x FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --highlight-line
 
-set -x LESS --quit-if-one-screen --RAW-CONTROL-CHARS
+set -x LESS --quit-if-one-screen --ignore-case --RAW-CONTROL-CHARS
 
 # https://github.com/sharkdp/bat/blob/2371077352b06954cc013e3a7830ae4021060d3b/README.md#man
 # TODO(bat v0.26.2): Simplify to `bat --plain --language=man` (https://github.com/sharkdp/bat/blob/59c58969024d48c0575c42250c7dd87a70106fbd/README.md#man)
