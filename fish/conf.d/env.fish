d07603dad74964d34fd636163f0fb72fcf6ee2a6
Justin Su <injustsu@gmail.com>
2024-09-20 03:04:36 -0400

    less: Move configuration from Git config to an environment variable

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -4,6 +4,9 @@ set -gx DIRENV_LOG_FORMAT
 set -gx FZF_DEFAULT_COMMAND fd
 set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --highlight-line --preview-window=wrap
 
+# Overrides Git's `LESS=FRX` default to unbreak mouse scrolling (https://github.com/gwsw/less/issues/445)
+set -gx LESS --quit-if-one-screen --RAW-CONTROL-CHARS
+
 set -gx MANPAGER sh -c "'col -bx | bat --language=man --style=plain'"
 
 # TODO(https://github.com/zyedidia/micro/issues/1127): Remove when micro checks the COLORTERM environment variable
