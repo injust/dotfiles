02cc68fe8a477752dcd781b79f775c8ea96a63bf
Justin Su <injustsu@gmail.com>
2024-09-20 03:32:18 -0400

    less: Configure

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -7,7 +7,7 @@ set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ -
 --color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --color=border:#313244,label:#cdd6f4
 
 # Overrides Git's `LESS=FRX` default to unbreak mouse scrolling (https://github.com/gwsw/less/issues/445)
-set -gx LESS --quit-if-one-screen --ignore-case --RAW-CONTROL-CHARS
+set -gx LESS --quit-if-one-screen --ignore-case --jump-target=.1 --LONG-PROMPT --quiet --RAW-CONTROL-CHARS --incsearch --no-vbell
 
 set -gx MANPAGER sh -c "'col -bx | bat --language=man --style=plain'"
 
