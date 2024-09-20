0aef2ebe5c16e9f7740d6a6f09cb7b40e2a6d1ed
Justin Su <injustsu@gmail.com>
2024-09-20 03:04:36 -0400

    less: Move default options from Git config to $LESS

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,6 +1,9 @@
 set -gx FZF_DEFAULT_COMMAND fd
 set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --highlight-line --preview-window=wrap
 
+# Override Git's `LESS=FRX` default to unbreak mouse scrolling (https://github.com/gwsw/less/issues/445)
+set -gx LESS --quit-if-one-screen --RAW-CONTROL-CHARS
+
 # https://github.com/sharkdp/bat/blob/f754f43e84a275d7a9a0ee3544c485eed747b311/README.md#man
 # TODO(bat v0.26.2): Simplify to `set -gx MANPAGER bat --plain --language=man` (https://github.com/sharkdp/bat/blob/59c58969024d48c0575c42250c7dd87a70106fbd/README.md#man)
 set -gx MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
