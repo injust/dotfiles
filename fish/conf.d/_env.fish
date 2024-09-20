44c33d007f63bcc832caae444e56e0dea10763b8
Justin Su <injustsu@gmail.com>
2024-09-20 03:32:18 -0400

    less: Configure

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -16,7 +16,8 @@ set -x COLORTERM truecolor
 set -x FZF_DEFAULT_COMMAND fd
 set -x FZF_DEFAULT_OPTS_FILE $XDG_CONFIG_HOME/.fzfrc
 
-set -x LESS --quit-if-one-screen --ignore-case --RAW-CONTROL-CHARS
+set -x LESS --quit-if-one-screen --ignore-case --jump-target=.1 --LONG-PROMPT --quiet --RAW-CONTROL-CHARS --incsearch --no-vbell
+set -x LESSSECURE 1
 # https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings
 set -x LESSUTFCHARDEF e000-e09f:w,e0a0-e0bf:p,e0c0-f8ff:w,f0001-fffff:w
 
