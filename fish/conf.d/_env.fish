d9d115093bcea4accff1534628daf4241f55a9c5
Justin Su <injustsu@gmail.com>
2024-09-20 03:19:28 -0400

    Use smart-case search

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -1,7 +1,7 @@
 set -x FZF_DEFAULT_COMMAND fd
 set -x FZF_DEFAULT_OPTS --layout=reverse --multi --highlight-line --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --scrollbar=▐ --prompt=❯
 
-set -x LESS --quit-if-one-screen --RAW-CONTROL-CHARS
+set -x LESS --quit-if-one-screen --ignore-case --RAW-CONTROL-CHARS
 
 # https://github.com/sharkdp/bat/blob/master/README.md#man
 set -x MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
