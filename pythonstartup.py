d9e716d595353bf46fdf1fd4f65c90b18e0a7ccd
Justin Su <injustsu@gmail.com>
2024-12-04 05:18:42 -0500

    Add `PYTHONSTARTUP` script

---
+++ pythonstartup.py
@@ -0,0 +1,17 @@
+from importlib import reload as reload  # noqa: PLC0414
+
+try:
+    from rich import inspect as _inspect
+    from rich import pretty
+    from rich import print as print  # noqa: PLC0414
+except ModuleNotFoundError:
+    pass
+else:
+    from functools import partial
+
+    pretty.install()
+    del pretty
+
+    inspect = partial(_inspect, methods=True)
+    del _inspect
+    del partial
