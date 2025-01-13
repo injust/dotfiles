0cc0da8016aa46e422e87fda48cfa09708b77cbd
Justin Su <injustsu@gmail.com>
2025-01-12 19:30:21 -0500

    Homebrew: Configure

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,6 +1,9 @@
 set -gx FZF_DEFAULT_COMMAND fd
 set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --highlight-line
 
+set -gx HOMEBREW_NO_ANALYTICS 1 # In addition to `brew analytics off`
+set -gx HOMEBREW_NO_ENV_HINTS 1
+
 # Override Git's `LESS=FRX` default to unbreak mouse scrolling (https://github.com/gwsw/less/issues/445)
 set -gx LESS --quit-if-one-screen --ignore-case --jump-target=.1 --LONG-PROMPT --quiet --RAW-CONTROL-CHARS --incsearch --no-vbell
 
