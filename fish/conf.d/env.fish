b8222f4013e72e11e6a1207b651065be89e827bf
Justin Su <injustsu@gmail.com>
2025-01-12 19:30:21 -0500

    Homebrew: Configure

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,6 +1,9 @@
 set -x FZF_DEFAULT_COMMAND fd
 set -x FZF_DEFAULT_OPTS --layout=reverse --multi --highlight-line --marker=▌ --marker-multi-line=▖▌▘ --ellipsis=… --tabstop=4 --scrollbar=▐ --prompt=❯
 
+set -x HOMEBREW_NO_ANALYTICS 1 # In addition to `brew analytics off`
+set -x HOMEBREW_NO_ENV_HINTS 1
+
 set -x LESS --quit-if-one-screen --ignore-case --jump-target=.1 --LONG-PROMPT --quiet --RAW-CONTROL-CHARS --incsearch --no-vbell
 set -x LESSSECURE 1
 
