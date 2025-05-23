ce5118c0dc9087b4f8091661de47d534c993ba42
Justin Su <injustsu@gmail.com>
2025-05-23 00:11:46 -0400

    Switch from GNU coreutils to uutils

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,4 +1,3 @@
-fish_add_path -ag /usr/local/opt/coreutils/libexec/gnubin
 fish_add_path -ag /usr/local/opt/curl/bin
 fish_add_path -ag /usr/local/opt/file-formula/bin
 fish_add_path -ag /usr/local/opt/findutils/libexec/gnubin
@@ -9,3 +8,4 @@ fish_add_path -ag /usr/local/opt/gnu-units/libexec/gnubin
 fish_add_path -ag /usr/local/opt/gnu-which/libexec/gnubin
 fish_add_path -ag /usr/local/opt/grep/libexec/gnubin
 fish_add_path -ag /usr/local/opt/make/libexec/gnubin
+fish_add_path -ag /usr/local/opt/uutils-coreutils/libexec/uubin
