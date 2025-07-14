563f96a3527aa9a7c4600c8608def693b74b6647
Justin Su <injustsu@gmail.com>
2025-07-14 17:59:50 -0400

    fish: Replace `caffeinate` in title with ☕

--- fish/conf.d/title.fish
+++ fish/conf.d/title.fish
@@ -1,6 +1,7 @@
 status is-interactive; or exit
 
 # https://github.com/fish-shell/fish-shell/blob/9870faa8beceb8807a79802cb6ff457e7a3bf956/share/functions/fish_title.fish:
+# - Replace "caffeinate" with ☕
 # - Unshorten command, hostname, and pwd
 function fish_title
     # If we're connected via SSH, we print the hostname
@@ -11,7 +12,11 @@ function fish_title
     if set -q argv[1]
         # An override for the current command is passed as the first parameter
         # This is used by `fg` to show the true process name, among others
-        set command $argv[1]
+        set -l command_override $argv[1]
+        if test "$command" = caffeinate
+            set command_override (string replace --regex --max-matches=1 '^caffeinate\b' ☕ -- $command_override)
+        end
+        set command $command_override
     else if test "$command" = fish
         # Don't print "fish" because it's redundant
         set command
