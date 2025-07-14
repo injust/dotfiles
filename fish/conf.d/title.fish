d2972d7b5dbcabfeb0ef2575559db154ae4baa5e
Justin Su <injustsu@gmail.com>
2025-07-14 13:26:02 -0400

    fish: Unshorten command, hostname, and pwd in title

---
+++ fish/conf.d/title.fish
@@ -0,0 +1,21 @@
+status is-interactive; or exit
+
+# https://github.com/fish-shell/fish-shell/blob/9870faa8beceb8807a79802cb6ff457e7a3bf956/share/functions/fish_title.fish:
+# - Unshorten command, hostname, and pwd
+function fish_title
+    # If we're connected via SSH, we print the hostname
+    set -l ssh
+    set -q SSH_TTY; and set ssh "[$(prompt_hostname)]"
+
+    set -l command (status current-command)
+    if set -q argv[1]
+        # An override for the current command is passed as the first parameter
+        # This is used by `fg` to show the true process name, among others
+        set command $argv[1]
+    else if test "$command" = fish
+        # Don't print "fish" because it's redundant
+        set command
+    end
+
+    echo -- $ssh $command (prompt_pwd --dir-length=0)
+end
