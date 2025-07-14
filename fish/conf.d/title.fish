f0f0fd7c86aea0262ae94874ad2d9d0fdf38b895
Justin Su <injustsu@gmail.com>
2025-07-14 13:26:02 -0400

    fish: Unshorten `fish_title`

---
+++ fish/conf.d/title.fish
@@ -0,0 +1,23 @@
+if not status is-interactive
+    exit
+end
+
+# https://github.com/fish-shell/fish-shell/blob/9870faa8beceb8807a79802cb6ff457e7a3bf956/share/functions/fish_title.fish but unshortened
+function fish_title
+    # If we're connected via ssh, we print the hostname.
+    set -l ssh
+    set -q SSH_TTY
+    and set ssh "[$(prompt_hostname)]"
+    # An override for the current command is passed as the first parameter.
+    # This is used by `fg` to show the true process name, among others.
+    if set -q argv[1]
+        echo -- $ssh $argv[1] (prompt_pwd --dir-length=0)
+    else
+        # Don't print "fish" because it's redundant
+        set -l command (status current-command)
+        if test "$command" = fish
+            set command
+        end
+        echo -- $ssh $command (prompt_pwd --dir-length=0)
+    end
+end
