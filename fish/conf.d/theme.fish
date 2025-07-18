cb21fd412536913ee38a64cbf722892b95ee1934
Justin Su <injustsu@gmail.com>
2025-07-18 09:19:23 -0400

    fish: Automatically switch between light/dark theme

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -2,10 +2,17 @@ status is-interactive; or exit
 
 fish_config theme choose catppuccin-mocha
 
-# Remove `bg` color to support transparent terminal background
-set -a FZF_DEFAULT_OPTS (string replace --regex 'bg:#[[:xdigit:]]{6},' '' <(status dirname)/../../catppuccin/fzf/themes/catppuccin-fzf-mocha.rc)
+function apply_theme --on-variable=fish_terminal_color_theme
+    set -f flavor (test $fish_terminal_color_theme = light; and echo latte; or echo mocha)
 
-# https://github.com/junegunn/fzf/issues/4549
-set -a fzf_preview_file_cmd --theme=(command -q /usr/bin/defaults; and echo auto:system; or echo dark)
+    set -gx BAT_THEME $fish_terminal_color_theme
 
-set -x LS_COLORS (vivid generate catppuccin-mocha)
+    set -gx DELTA_FEATURES +catppuccin-$flavor
+
+    # Remove `bg` color to support transparent terminal background
+    set -a FZF_DEFAULT_OPTS (string replace --regex 'bg:#[[:xdigit:]]{6},' '' <(status dirname)/../../catppuccin/fzf/themes/catppuccin-fzf-$flavor.rc)
+
+    set -gx LS_COLORS (vivid generate catppuccin-$flavor)
+
+    starship config palette catppuccin_$flavor
+end
