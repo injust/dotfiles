4b434237202c73d65218fc16b0c5fa522209f29c
Justin Su <injustsu@gmail.com>
2025-07-22 21:11:06 -0400

    fish: Add `yt-dlp` alias for default options

---
+++ fish/functions/yt-dlp.fish
@@ -0,0 +1,3 @@
+function yt-dlp --description='alias yt-dlp=yt-dlp --mtime --embed-subs --embed-thumbnail --embed-metadata --xattrs'
+    command yt-dlp --mtime --embed-subs --embed-thumbnail --embed-metadata --xattrs $argv
+end
