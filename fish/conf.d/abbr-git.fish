24e95d3d41ce24f9cfa03a21520c1a4e98e6cb78
Justin Su <injustsu@gmail.com>
2025-08-10 04:04:33 -0400

    git-spice: Add `gsrr` abbreviation for `gs repo restack`

--- fish/conf.d/abbr-git.fish
+++ fish/conf.d/abbr-git.fish
@@ -85,6 +85,8 @@ abbr --command=git sh -- show -p --stat
 abbr gsht -- git showt
 abbr --command=git sht -- showt
 
+abbr gsrr -- gs repo restack
+
 abbr gst -- git status
 abbr --command=git st -- status
 
