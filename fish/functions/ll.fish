f4b3db45adf0dacee31a3a96cdd8ce7bced51950
Justin Su <injustsu@gmail.com>
2025-08-12 08:42:18 -0400

    eza: Replace $HOME with ~
    https://redirect.github.com/eza-community/eza/issues/1584

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,3 +1,13 @@
 function ll --description='alias ll=la -l --smart-group --git-repos' --wraps=eza
-    la --long --smart-group --git-repos $argv
+    set -f options --long --smart-group --git-repos
+
+    if isatty stdout
+        set -a options --classify=always --color=always --icons=always
+
+        # https://github.com/eza-community/eza/issues/1584
+        la $options $argv | string replace --regex "(->.+)$HOME/" '$1~/'
+        return $pipestatus[1]
+    else
+        la $options $argv
+    end
 end
