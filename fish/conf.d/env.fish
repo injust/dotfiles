e2db8fb92b43fee96eee78ccb7f6684bff597c7a
Justin Su <injustsu@gmail.com>
2025-08-16 07:21:03 -0400

    hwatch: Configure default options

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -4,6 +4,8 @@ set -gx FZF_DEFAULT_OPTS --multi --layout=reverse --scrollbar=▐ --prompt=❯ -
 set -gx HOMEBREW_NO_ANALYTICS 1 # In addition to `brew analytics off`
 set -gx HOMEBREW_NO_ENV_HINTS 1
 
+set -gx HWATCH --beep --mouse --color --differences
+
 # Override Git's `LESS=FRX` default to unbreak mouse scrolling (https://github.com/gwsw/less/issues/445)
 set -gx LESS --quit-if-one-screen --ignore-case --jump-target=.1 --LONG-PROMPT --quiet --RAW-CONTROL-CHARS --incsearch --no-vbell
 
