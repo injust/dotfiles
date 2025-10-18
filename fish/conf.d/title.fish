14132631c83885b0dcaa25224b27afb0a3bfd34f
Justin Su <injustsu@gmail.com>
2025-10-18 06:49:47 -0400

    fish: Strip leading whitespace from title

--- fish/conf.d/title.fish
+++ fish/conf.d/title.fish
@@ -2,6 +2,7 @@ status is-interactive; or exit
 
 # https://github.com/fish-shell/fish-shell/blob/9870faa8beceb8807a79802cb6ff457e7a3bf956/share/functions/fish_title.fish:
 # - Replace "caffeinate" with ☕
+# - Strip leading whitespace from $argv[1]
 # - Unshorten command, hostname, and pwd
 function fish_title
     # If we're connected via SSH, we print the hostname
@@ -12,7 +13,7 @@ function fish_title
     if set -q argv[1]
         # An override for the current command is passed as the first parameter
         # This is used by `fg` to show the true process name, among others
-        set -l command_override $argv[1]
+        set -l command_override (string trim --left -- $argv[1])
         if test "$command" = caffeinate
             set command_override (string replace --regex --max-matches=1 '^caffeinate\b' ☕ -- $command_override)
         end
