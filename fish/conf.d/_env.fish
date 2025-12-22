bfbe37586c6f5f7ae44de9a320c8f73d8a715014
Justin Su <injustsu@gmail.com>
2025-12-22 09:08:27 -0500

    screen: Configure

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -16,6 +16,8 @@ set -x PYTHONSTARTUP ~/.config/.pythonrc.py
 
 set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/config
 
+set -x SCREENRC ~/.config/.screenrc
+
 set -x UV_PYTHON_PREFERENCE only-managed
 set -x UV_VENV_CLEAR 1
 
