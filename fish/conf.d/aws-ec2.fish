65f77d69ea3c30821c4d3a1a4c8b707dca87a8ce
Justin Su <injustsu@gmail.com>
2026-01-04 09:35:35 -0500

    Add dotfiles for AWS EC2

---
+++ fish/conf.d/aws-ec2.fish
@@ -0,0 +1,8 @@
+set -l board_vendor_file /sys/devices/virtual/dmi/id/board_vendor
+test -f $board_vendor_file; and read -l board_vendor <$board_vendor_file; and test $board_vendor = 'Amazon EC2'; or exit
+
+if not set -q AWS_REGION
+    set -l imds_url http://169.254.169.254/latest
+    set -l token (curl -fsS -X PUT $imds_url/api/token -H 'X-AWS-EC2-Metadata-Token-TTL-Seconds: 5')
+    set -x AWS_REGION (curl -fsS $imds_url/meta-data/placement/region -H "X-AWS-EC2-Metadata-Token: $token")
+end
