f119d6d6ea8de4ef02ce5b16bc5d80fef4abd9ab
Justin Su <injustsu@gmail.com>
2026-01-04 09:35:35 -0500

    Add dotfiles for AWS EC2

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,4 +1,5 @@
 # Homebrew
 fish_add_path -g --path /usr/local/opt/{curl,file-formula}/bin /usr/local/opt/{findutils,gawk,gnu-{sed,tar,which},grep}/libexec/gnubin /usr/local/opt/uutils-coreutils/libexec/uubin
+fish_add_path -g --path /home/linuxbrew/.linuxbrew/bin
 
 fish_add_path -g ~/.local/bin ~/code/scripts
