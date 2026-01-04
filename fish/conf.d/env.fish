05df36b45324fc38c079a77d0603c2c62404fb21
Justin Su <injustsu@gmail.com>
2026-01-04 13:04:24 -0500

    TODO(bat v0.26.2): Simplify $MANPAGER
    https://redirect.github.com/sharkdp/bat/pull/3517

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -15,9 +15,8 @@ set -gx LESSSECURE 1
 # https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings
 set -gx LESSUTFCHARDEF e000-e09f:w,e0a0-e0bf:p,e0c0-f8ff:w,f0001-fffff:w
 
-# https://github.com/sharkdp/bat/blob/2371077352b06954cc013e3a7830ae4021060d3b/README.md#man
-# TODO(bat v0.26.2): Simplify to `bat --plain --language=man` (https://github.com/sharkdp/bat/blob/59c58969024d48c0575c42250c7dd87a70106fbd/README.md#man)
-set -gx MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
+# https://github.com/sharkdp/bat/blob/59c58969024d48c0575c42250c7dd87a70106fbd/README.md#man
+set -gx MANPAGER bat --plain --language=man
 
 set -gx PRE_COMMIT_ALLOW_NO_CONFIG 1
 
