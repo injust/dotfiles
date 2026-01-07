68ed19e39666d85bca2babf64b25738985deb9e3
Justin Su <injustsu@gmail.com>
2026-01-07 14:15:19 -0500

    fish: Add `mkcd` function

---
+++ fish/functions/mkcd.fish
@@ -0,0 +1,11 @@
+function mkcd --description='Create a directory and set CWD'
+    command mkdir $argv
+
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+            case '*'
+                cd $argv[(count $argv)]
+        end
+    end
+end
