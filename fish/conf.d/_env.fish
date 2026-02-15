41a22e93cabf1f9f2fb1cefbbb524894fb5eeff9
Justin Su <injustsu@gmail.com>
2026-02-14 23:55:16 -0500

    Install modern versions of Bash, curl, file, less, and OpenSSH

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -2,6 +2,7 @@ set -x XDG_CONFIG_HOME ~/code/dotfiles
 
 # Homebrew
 fish_add_path --path \
+    /usr/local/opt/{curl,file-formula}/bin \
     /usr/local/opt/{coreutils,findutils,gawk,gnu-{sed,tar,which},grep}/libexec/gnubin
 # User paths
 fish_add_path -g \
