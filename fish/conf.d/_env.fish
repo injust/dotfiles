a5338513f8ebff765f654781afd9272c5d049552
Justin Su <injustsu@gmail.com>
2026-03-01 12:31:48 -0500

    less: Treat Nerd Font PUA characters as printable
    https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -8,6 +8,8 @@ set -x HWATCH --beep --mouse --color --differences
 
 set -x LESS --quit-if-one-screen --ignore-case --jump-target=.1 --LONG-PROMPT --quiet --RAW-CONTROL-CHARS --incsearch --no-vbell
 set -x LESSSECURE 1
+# https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings
+set -x LESSUTFCHARDEF e000-e09f:w,e0a0-e0bf:p,e0c0-f8ff:w,f0001-fffff:w
 
 # https://github.com/sharkdp/bat/blob/master/README.md#man
 set -x MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat --plain --language=man'"
