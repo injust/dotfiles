22d87110e0af2917788424ef9856eb011256f03c
Justin Su <injustsu@gmail.com>
2026-03-01 22:17:40 -0500

    Git: Add `git sync` and `git watch-sync` commands

--- fish/conf.d/abbr-git.fish
+++ fish/conf.d/abbr-git.fish
@@ -115,3 +115,6 @@ abbr --command=git sw -- switch
 
 abbr gswc -- gs branch create --no-commit
 abbr --command=git swc -- switch -c
+
+abbr gws -- git watch-sync
+abbr --command=git ws -- watch-sync
