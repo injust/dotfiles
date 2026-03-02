6437eb0dce4eec0c352d59ba1b925580cebc33df
Justin Su <injustsu@gmail.com>
2026-03-01 22:17:40 -0500

    Git: Add `watch-sync` command

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,4 +1,4 @@
 # Homebrew
 fish_add_path -g --path /usr/local/opt/curl/bin /usr/local/opt/file-formula/bin /usr/local/opt/findutils/libexec/gnubin /usr/local/opt/gawk/libexec/gnubin /usr/local/opt/gnu-sed/libexec/gnubin /usr/local/opt/gnu-tar/libexec/gnubin /usr/local/opt/gnu-units/libexec/gnubin /usr/local/opt/gnu-which/libexec/gnubin /usr/local/opt/grep/libexec/gnubin /usr/local/opt/uutils-coreutils/libexec/uubin
 
-fish_add_path -g ~/.local/bin ~/code/scripts /home/linuxbrew/.linuxbrew/bin
+fish_add_path -g ~/.config/git/commands ~/.local/bin ~/code/scripts /home/linuxbrew/.linuxbrew/bin
