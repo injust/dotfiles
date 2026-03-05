16a47ea7530395122dd412bd0c94d7f0a5f42e19
Justin Su <injustsu@gmail.com>
2026-03-05 15:23:35 -0500

    Starship: Work around `add_newline` adding an empty line at shell startup
    https://redirect.github.com/starship/starship/issues/560#issuecomment-2409922650

--- fish/conf.d/starship.fish
+++ fish/conf.d/starship.fish
@@ -1,3 +1,14 @@
 status is-interactive; or exit
 
 starship init fish | source
+
+# https://github.com/starship/starship/issues/560#issuecomment-2409922650
+
+function clear --description='alias clear=command clear; commandline -f clear-screen'
+    command clear
+    commandline --function clear-screen
+end
+
+function prompt_newline --on-event=fish_postexec
+    echo
+end
