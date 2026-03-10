285d98313d2d125315956f62444983be7fe76b37
Justin Su <injustsu@gmail.com>
2026-03-10 18:41:55 -0400

    zmx: Install session manager (zsm) and add ctrl-p keybind

--- fish/conf.d/keybinds.fish
+++ fish/conf.d/keybinds.fish
@@ -1,5 +1,7 @@
 status is-interactive; or exit
 
+bind ctrl-p zsm
+
 # Override platform-specific keybinds (https://github.com/fish-shell/fish-shell/issues/10926#issue-2753198774)
 bind alt-left prevd-or-backward-word
 bind alt-right nextd-or-forward-word
