493463f01a1986c13d941332a9d5180f6550ac28
Justin Su <injustsu@gmail.com>
2026-03-28 17:29:31 -0400

    bat: Add global `--help` abbreviation for colorizing pager
    https://github.com/sharkdp/bat/blob/master/README.md#highlighting---help-messages

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -4,3 +4,7 @@ abbr find -- fd
 abbr grep -- rg
 abbr where -- type -a
 abbr xxhash -- xxh3sum
+
+# https://github.com/sharkdp/bat/blob/master/README.md#highlighting---help-messages
+abbr --position=anywhere -- -help '-help | help_pager'
+abbr --position=anywhere -- --help '--help | help_pager'
