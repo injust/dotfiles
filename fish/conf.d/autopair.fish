5d5be4fbeeb826108bb5866d57c084368360d092
Justin Su <injustsu@gmail.com>
2026-03-28 16:40:26 -0400

    fish: Enable autopair for backticks

--- fish/conf.d/autopair.fish
+++ fish/conf.d/autopair.fish
@@ -1,4 +1,8 @@
 status is-interactive; or exit
 
+set -a autopair_left `
+set -a autopair_right `
+set -a autopair_pairs ``
+
 # Use autopair for shift-tab completion (with pager search)
 bind shift-tab _autopair_shift_tab
