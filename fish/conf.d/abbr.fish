5527b97b72df75dfaf8e3cffb1ffbfc76d472bc9
Justin Su <injustsu@gmail.com>
2026-03-28 17:29:31 -0400

    bat: Colorize `--help` messages
    https://github.com/sharkdp/bat/blob/master/README.md#highlighting---help-messages

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -5,3 +5,6 @@ abbr grep -- rg
 abbr where -- type -a
 abbr which -- type
 abbr xxhash -- xxh3sum
+
+# https://github.com/sharkdp/bat/blob/master/README.md#highlighting---help-messages
+abbr --position=anywhere -- --help '--help | bat --plain --language=help'
