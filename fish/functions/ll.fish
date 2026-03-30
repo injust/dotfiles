0f2ebed8964b52ca11d0f8741491a2ddc150744d
Justin Su <injustsu@gmail.com>
2026-03-30 19:06:47 -0400

    TODO(eza v0.24.0): Add `--hyperlink=always` to `ll` alias
    https://redirect.github.com/eza-community/eza/pull/1664

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -2,8 +2,7 @@ function ll --description='alias ll=la -l --smart-group --git-repos' --wraps=eza
     set -l options --long --smart-group --git-repos
 
     if isatty stdout
-        # TODO(https://github.com/eza-community/eza/issues/1093): Add `--hyperlink=always`
-        set -a options --classify=always --color=always --icons=always
+        set -a options --classify=always --color=always --icons=always --hyperlink=always
 
         # https://github.com/eza-community/eza/issues/1584
         la $options $argv | string replace --regex "(->.+)$HOME/" '$1~/'
