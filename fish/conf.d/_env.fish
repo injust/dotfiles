8eca3b4aacc6d330f4eb4d33cb80952f228a6e3f
Justin Su <injustsu@gmail.com>
2026-08-08 19:53:31 -0400

    less: Configure

--- fish/conf.d/_env.fish
+++ fish/conf.d/_env.fish
@@ -19,7 +19,8 @@ set -x HOMEBREW_NO_ANALYTICS 1
 set -x HOMEBREW_NO_ENV_HINTS 1
 set -x HOMEBREW_NO_UPGRADE_QUIT_CASKS 1
 
-set -x LESS --quit-if-one-screen --ignore-case --RAW-CONTROL-CHARS
+set -x LESS --quit-if-one-screen --ignore-case --jump-target=.1 --LONG-PROMPT --quiet --RAW-CONTROL-CHARS --incsearch --no-vbell
+set -x LESSSECURE 1
 # https://github.com/ryanoasis/nerd-fonts/wiki/FAQ-and-Troubleshooting#less-settings
 set -x LESSUTFCHARDEF e000-e09f:w,e0a0-e0bf:p,e0c0-f8ff:w,f0001-fffff:w
 
