550a77414910d0792f48dd13d8ffc9d1348f1609
Justin Su <injustsu@gmail.com>
2026-08-25 03:15:25 -0400

    fish: Omit commits from Git tab completions
    https://github.com/fish-shell/fish-shell/discussions/12582

---
+++ fish/completions/git.fish
@@ -0,0 +1,9 @@
+# https://github.com/fish-shell/fish-shell/discussions/12582
+
+status get-file completions/git.fish | source
+
+function __fish_git_commits
+end
+
+function __fish_git_recent_commits
+end
